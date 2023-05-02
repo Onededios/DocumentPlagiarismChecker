c1f8b2fc74fca5d297707f7ed8cf3b9710b41cf7
Kevin <amunozgil@elpuig.xeill.net>
2023-05-02 20:34:18 +0200

    Comentario insertado con exito

--- src/Comparators/ParagraphWordCounter/Comparator.cs
+++ src/Comparators/ParagraphWordCounter/Comparator.cs
@@ -34,7 +34,8 @@ namespace DocumentPlagiarismChecker.Comparators.ParagraphWordCounter
         /// Counts how many words and how many times appears within each paragraph in a document, and checks the matching percentage.
         /// </summary>
         /// <returns>The matching's results.</returns>
-        public override ComparatorMatchingScore Run(){     
+        public override ComparatorMatchingScore Run(){
+            //Inserta comentario generico en este lugar. Muchas gracias.     
             //This order is meant to improving performance
             ExcludeSampleExactMatches(); 
             ExcludeSamplePartialMatches(this.Left, 0.70f);  //TODO: threshold value must be get from settings; check if can be removed
